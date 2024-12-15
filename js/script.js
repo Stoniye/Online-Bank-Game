152d8b16a834b9511c8776b7074fc81c5745837b
Stonie <aronstein10@gmail.com>
2024-12-15 11:58:26 +0100

    Created Project
    Created the Project and Folder struct
    Created prototype of login window

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const login_input = document.querySelectorAll('.input-login');
+
+function login_clicked() {
+    <!--
+     Login with:
+     login_input[0].value == E-Mail
+     login_input[1].value == Password
+     -->
+}
